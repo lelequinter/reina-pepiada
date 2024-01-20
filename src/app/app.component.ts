f93fba6e20cc9fa8fc6c4eb126a714f95d109dd9
Leyder Quintero <desarrollador9@wedevel.co>
2024-01-20 18:59:45 -0500

    Maquetar la base de la navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzAffixModule } from 'ng-zorro-antd/affix';
+import { CrownOutline } from '@ant-design/icons-angular/icons';
 
+const icons: IconDefinition[] = [ CrownOutline ];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [
+    CommonModule,
+    NzAffixModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isCollapsed = true;
+  private readonly iconSvc = inject(NzIconService);
+
+  constructor(){
+    this.iconSvc.addIcon(CrownOutline);
+  }
+
 }

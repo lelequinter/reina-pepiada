4f54139c686b8ea5db592204b950329cfc24213b
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-24 16:35:13 -0500

    maquetacion de  product-card

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { NzCardModule } from "ng-zorro-antd/card";
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [
     NzCardModule,
     NzCarouselModule,
+    ProductCardComponent
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
